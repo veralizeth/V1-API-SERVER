ae70ab227b92dbb1ce21d6b31ed05f9264546b90
Lizeth Vera <lizethvera@Lizeths-MacBook-Pro.local>
2024-10-22 13:33:05 -0700

    Adding put route and get, failing get test

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const express = require('express')
+const crypto = require('crypto')
+
 const server = express()
 server.use(express.text())
+
+
 // The tests exercise the server by requiring it as a module,
 // rather than running it in a separate process and listening on a port
 module.exports = server
+
+const repositories = {};
+
+function toSha256Hex(content) {
+    return crypto.createHash('sha256').update(content).digest('hex');
+}
+
+server.put('/data/:repository', (req, res) => {
+    const repository = req.params.repository;
+    const content = req.body;
+
+    // Generate the hash for the content. 
+    const oid = toSha256Hex(content);
+    const size = Buffer.byteLength(content, 'utf8');
+
+    if (!repositories[repository]) {
+        repositories[repository] = {}
+    }
+
+    repositories[repository][oid] = content;
+    console.log('Stored repositories:', repositories);
+
+    res.status(201).json({ oid, size });
+
+}); 
+
+// Downloading the object
 server.get('/data/:repository/:objectID', (req, res) => {
-  res.status(200)
+    const repository = req.params.repository; 
+    const objectID = req.params.objectID;
+
+    console.log('Request to get object:', repository, objectID);
+
+    // Checking if the repo and objectID exists
+    if(repositories[repository] && repositories[repository][objectID]) {
+        res.status(200).send(repositories[repository][objectID]); 
+    } else {
+        res.status(404).send({message: 'Object not found'});
+    }    
 })
 
 if (require.main === module) {
-  // Start server only when we run this on the command line and explicitly ignore this while testing
-  const port = process.env.PORT || 3000
-  server.listen((port), () => {
-    console.log(`App listening at http://localhost:${port}`)
-  })
+    // Start server only when we run this on the command line and explicitly ignore this while testing
+    const port = process.env.PORT || 3000
+    server.listen((port), () => {
+        console.log(`App listening at http://localhost:${port}`)
+    })
 }
